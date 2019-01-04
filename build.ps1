f3551f93f1ea4fd8138a1efb058b1039e8116ee9
Brian Addicks <brian@addicks.us>
2019-01-04 17:39:30 -0500

    trying to get poshbot loaded in appveyor !deploy

--- build.ps1
+++ build.ps1
@@ -41,7 +41,7 @@ function Resolve-Module {
 # Grab nuget bits, install modules, set build variables, start build.
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
-Resolve-Module Psake, PSDeploy, Pester, BuildHelpers, PoshBot
+Resolve-Module PoshBot, Psake, PSDeploy, Pester, BuildHelpers
 
 Set-BuildEnvironment -Force
 
